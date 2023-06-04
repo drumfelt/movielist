059a74076400e227bdb2a93114be3c5c75673cc1
drumfelt <drumfelt1@my.apsu.edu>
2023-06-04 00:15:36 -0500

    removed unused imports and variables

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 import {
   InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
+  InteractionResponseType
 } from 'discord-interactions';
 import {
   VerifyDiscordRequest,
-  getRandomEmoji,
-  DiscordRequest,
+  getRandomEmoji
 } from './utils.js';
 import sqlite3 from 'sqlite3';
-import fs from 'fs';
 
 // Create an express app
 const app = express();
@@ -27,15 +22,12 @@ const db = new sqlite.Database(moviesDbFile);
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-// Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
-
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
 app.post('/interactions', async function (req, res) {
   // Interaction type and data
-  const { type, id, data } = req.body;
+  const { type, data } = req.body;
 
   /**
    * Handle verification requests
@@ -58,7 +50,7 @@ app.post('/interactions', async function (req, res) {
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
+          content: `hello world ${getRandomEmoji()}`,
         },
       });
     }
